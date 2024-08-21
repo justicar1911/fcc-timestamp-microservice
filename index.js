83d472d12dd9b31869774210b65365e935b89195
Nghia Nguyen <nthanhnghia1911@gmail.com>
2024-08-21 17:05:37 +0000

    passed all fcc test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,9 +21,30 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api", (req, res) => {
+  return res.json({
+    unix: new Date().valueOf(),
+    utc: new Date().toUTCString()
+  })
+})
+
+app.get("/api/:date", (req, res) => {
+  const { date } = req.params
+  let parseDate = isNaN(new Date(date)) ? Number(date) : date;
+  let isInvalidDate = new Date(parseDate).toString() === 'Invalid Date';
+
+  if (isInvalidDate) {
+    return res.json({ error: "Invalid Date" })
+  }
+
+  return res.json({
+    unix: new Date(parseDate).valueOf(),
+    utc: new Date(parseDate).toUTCString()
+  })
+})
 
 
 // Listen on port set in environment variable or default to 3000
